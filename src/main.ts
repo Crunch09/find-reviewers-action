1c06db9efa777afc3a1c44054137ceab255481e4
Florian Thomas <flo@florianthomas.net>
2020-06-20 13:07:56 +0100

    move to a typescript action
    It generally seems easier to run TS/JS actions than Docker ones.

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+const nameToGreet = "Foobar";
+console.log(`Hello ${nameToGreet}`);
+const time = new Date().toTimeString();
+core.setOutput("time", time);
+
+const payload = JSON.stringify(github.context.payload, undefined, 2);
+console.log(`The event payload: ${payload}`);
